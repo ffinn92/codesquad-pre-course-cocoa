ed3a769db1d1162ec1694e3efe874aea8e98ded9
ffinn92 <92678171+ffinn92@users.noreply.github.com>
2021-11-01 17:33:00 +0900

    code test update

--- d1 mission/Gugudan.java
+++ d1 mission/Gugudan.java
@@ -4,10 +4,10 @@ public class Gugudan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // 시작단과 끝단을 입력 받는다.
 
-        int a = sc.nextInt(); // 시작단 입력받을 변수
-        int b = sc.nextInt(); // 끝단 입력받을 변수
+        int c = sc.nextInt(); // 시작단 입력받을 변수
+        int d = sc.nextInt(); // 끝단 입력받을 변수
 
-        for(int i=a; i<=b; i++){
+        for(int i=c; i<=d; i++){
             for(int j=1; j<10; j++){
                 System.out.println(i+"x"+j);
             }
