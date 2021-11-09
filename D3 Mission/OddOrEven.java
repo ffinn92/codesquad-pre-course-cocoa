e34a53320ea35cf63aa703217fb0435310b3318f
ffinn92 <92678171+ffinn92@users.noreply.github.com>
2021-11-09 17:57:25 +0900

    전체적으로 변수명을 수정

--- D3 Mission/OddOrEven.java
+++ D3 Mission/OddOrEven.java
@@ -4,60 +4,60 @@ import static java.lang.Math.min;
 import static java.lang.Math.pow;
 
 public class OddOrEven {
+
+    Scanner sc = inputPlayerName();
+
+    int playerCapital = 100;
+    int computerCapital = 120;
+    int stageCount = 1;
+
     public static void main(String[] args) {
         OddOrEven oddoreven = new OddOrEven();
         oddoreven.play();
     }
 
-    Scanner sc = inputPlayeName();
-
-    int pnameCapital = 100;
-    int rnameCpatial = 120;
-    int roundcount = 1;
-
     private int play() {
 
-        int rCountNum = rCountNum();
+        int computerRandomNum = selectComputerRandomNum();
 
-        String pInputOddEven = inputOddEven(sc, "홀,짝을 입력해주세요.");
-        int bettingNum = inputBetnumover(sc, pnameCapital, rnameCpatial);
+        String playerInputOddEven = inputOddEven(sc, "홀,짝을 입력해주세요.");
+        int betMoney = checkBetMoney(sc, playerCapital, computerCapital);
 
-        String rOddEven = rCountnumJudge(rCountNum);
+        String computerOddEven = checkOddEven(computerRandomNum);
 
-
-        if (pInputOddEven.equals(rOddEven)) {
-            rnameCpatial = rBalanceLose(rnameCpatial, bettingNum, "현재 상대방의 잔액은 ");
-            pnameCapital = pBalanceWin(pnameCapital, bettingNum, "현재 나의 잔액은 ");
+        if (playerInputOddEven.equals(computerOddEven)) {
+            computerCapital = computerLose(computerCapital, betMoney, "현재 상대방의 잔액은 ");
+            playerCapital = playerWin(playerCapital, betMoney, "현재 나의 잔액은 ");
         } else {
-            pnameCapital = pBalanceLose(pnameCapital, bettingNum);
-            rnameCpatial = rBalanceWin(rnameCpatial, bettingNum, "현재 상대방의 잔액은 ");
+            playerCapital = playerLose(playerCapital, betMoney);
+            computerCapital = computerWin(computerCapital, betMoney, "현재 상대방의 잔액은 ");
         }
 
-        if (roundcount == 8 && rnameCpatial == 0) {
-            playerWin(pnameCapital, roundcount);
-        } else if (rnameCpatial == 0) {
-            roundcount++;
-            rnameCpatial = nextLevel(pnameCapital, roundcount);
+        if (stageCount == 8 && computerCapital == 0) {
+            playerWin(playerCapital, stageCount);
+        } else if (computerCapital == 0) {
+            stageCount++;
+            computerCapital = nextLevel(playerCapital, stageCount);
             return play();
-        } else if (pnameCapital == 0) {
-            playerlose(pnameCapital, roundcount);
+        } else if (playerCapital == 0) {
+            playerlose(playerCapital, stageCount);
         } else {
             return play();
         }
-        return pnameCapital;
+        return playerCapital;
     }
 
-    private static Scanner inputPlayeName() {
+    private static Scanner inputPlayerName() {
         Scanner sc = new Scanner(System.in);
-        String pname = inputOddEven(sc, "플레이어의 이름을 입력해주세요.");
-        System.out.println("플레이어의 이름은 " + pname + " 입니다.");
+        String playerName = inputOddEven(sc, "플레이어의 이름을 입력해주세요.");
+        System.out.println("플레이어의 이름은 " + playerName + " 입니다.");
         return sc;
     }
 
-    private static int rCountNum() {
-        int rCountNum;
-        rCountNum = (int) (Math.random() * 20) + 1;
-        return rCountNum;
+    private static int selectComputerRandomNum() {
+        int computerRandomNum;
+        computerRandomNum = (int) (Math.random() * 20) + 1;
+        return computerRandomNum;
     }
 
     private static String inputOddEven(Scanner sc, String s) {
@@ -65,75 +65,75 @@ public class OddOrEven {
         return sc.next();
     }
 
-    private static int inputBetnumover(Scanner sc, int pnameCapital, int rnameCpatial) {
-        int inputBetNum = getInputBetNum(sc);
+    private static int checkBetMoney(Scanner sc, int playerCapital, int computerCapital) {
+        int validBetMoney = getInputBetMoney(sc);
 
-        while (inputBetNum != 0) {
-            if (inputBetNum > min(pnameCapital, rnameCpatial)) {
-                System.out.println("베팅 금액을 초과하였습니다." + "\n" + min(pnameCapital, rnameCpatial) + " " + "이하로 입력해주세요.");
-                inputBetNum = getInputBetNum(sc);
+        while (validBetMoney != 0) {
+            if (validBetMoney > min(playerCapital, computerCapital)) {
+                System.out.println("베팅 금액을 초과하였습니다." + "\n" + min(playerCapital, computerCapital) + " " + "이하로 입력해주세요.");
+                validBetMoney = getInputBetMoney(sc);
             } else {
                 break;
             }
         }
-        return inputBetNum;
+        return validBetMoney;
     }
 
-    private static int getInputBetNum(Scanner sc) {
+    private static int getInputBetMoney(Scanner sc) {
         System.out.println("베팅 금액을 입력해주세요.");
-        int inputBetNum = sc.nextInt();
-        return inputBetNum;
+        int inputBetMoney = sc.nextInt();
+        return inputBetMoney;
     }
 
-    private static String rCountnumJudge(int rCountNum) {
-        String rOddEven;
+    private static String checkOddEven(int n) {
+        String computerOddEven;
 
-        if (rCountNum % 2 == 0) {
-            rOddEven = "짝";
+        if (n % 2 == 0) {
+            computerOddEven = "짝";
         } else {
-            rOddEven = "홀";
+            computerOddEven = "홀";
         }
-        return rOddEven;
+        return computerOddEven;
     }
 
-    private static int rBalanceLose(int rnameCpatial, int bettingNum, String s) {
-        rnameCpatial -= bettingNum;
-        System.out.println(s + rnameCpatial + "입니다.");
-        return rnameCpatial;
+    private static int computerLose(int computerCapital, int betMoney, String s) {
+        computerCapital -= betMoney;
+        System.out.println(s + computerCapital + "입니다.");
+        return computerCapital;
     }
 
-    private static int pBalanceWin(int pnameCapital, int bettingNum, String s) {
-        pnameCapital += bettingNum;
-        System.out.println(s + pnameCapital + "입니다.");
-        return pnameCapital;
+    private static int playerWin(int playerCapital, int betMoney, String s) {
+        playerCapital += betMoney;
+        System.out.println(s + playerCapital + "입니다.");
+        return playerCapital;
     }
 
-    private static int rBalanceWin(int rnameCpatial, int bettingNum, String s) {
-        rnameCpatial = pBalanceWin(rnameCpatial, bettingNum, s);
-        return rnameCpatial;
+    private static int computerWin(int computerCaptial, int betMoney, String s) {
+        computerCaptial = playerWin(computerCaptial, betMoney, s);
+        return computerCaptial;
     }
 
-    private static int pBalanceLose(int pnameCapital, int bettingNum) {
-        pnameCapital = rBalanceLose(pnameCapital, bettingNum, "현재 나의 잔액은 ");
-        return pnameCapital;
+    private static int playerLose(int playerCaptial, int betMoney) {
+        playerCaptial = computerLose(playerCaptial, betMoney, "현재 나의 잔액은 ");
+        return playerCaptial;
     }
 
 
-    private static void playerWin(int pnameCapital, int i) {
+    private static void playerWin(int playerCapital, int i) {
         System.out.println("WIN!!");
-        System.out.println("당신의 소지금은 " + pnameCapital + " 이며, 총 " + i + "라운드 진행되었습니다.");
+        System.out.println("당신의 소지금은 " + playerCapital + " 이며, 총 " + i + "라운드 진행되었습니다.");
     }
 
-    private static int nextLevel(int pnameCapital, int roundcount) {
-        int rnameCpatial;
-        rnameCpatial = (int) (pnameCapital * pow(1.2, roundcount));
-        System.out.println(roundcount + "라운드" + ">>" + "새로운 상대가 등장합니다.");
-        return rnameCpatial;
+    private static int nextLevel(int playerCapital, int roundCount) {
+        int computerCapital;
+        computerCapital = (int) (playerCapital * pow(1.2, roundCount));
+        System.out.println(roundCount + "라운드" + ">>" + "새로운 상대가 등장합니다.");
+        return computerCapital;
     }
 
-    private static void playerlose(int pnameCapital, int roundcount) {
+    private static void playerlose(int playerCaptial, int roundCount) {
 
-        System.out.println("당신의 소지금은 " + pnameCapital + " 이며, 총 " + roundcount + "라운드 진행되었습니다.");
+        System.out.println("당신의 소지금은 " + playerCaptial + " 이며, 총 " + roundCount + "라운드 진행되었습니다.");
         System.out.println(" - Game Over - ");
     }
 
