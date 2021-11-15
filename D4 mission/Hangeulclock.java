874652906da82553fa74841714c21ca0fe5053c7
ffinn92 <92678171+ffinn92@users.noreply.github.com>
2021-11-15 15:35:37 +0900

    println 으로 Bold 출력 테스트

--- D4 mission/Hangeulclock.java
+++ D4 mission/Hangeulclock.java
@@ -2,39 +2,46 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Hangeulclock {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_Gray = "\u001B[37m";
+
+    public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
+    public static final String RED_BOLD_BRIGHT = "\033[1;91m";   // RED
+    public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
+    public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
+    public static final String BLUE_BOLD_BRIGHT = "\033[1;94m";  // BLUE
+    public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
+    public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";  // CYAN
+    public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
+
+
     public static void main(String[] args) {
         //1.다차원 배열로 한글을 입력하여 출력보드를 만든다.
         //2.현재 시간을 1분 단위로 입력받는다.
         //3.각 시간(숫자)과 한글표기가 일치할때 볼드처리를 해준다.
         //3-1.일치하는지 찾는 방법은 스위치 케이스 문으로 하나씩 확인해본다.
         //2~3번을 반복한다.
+
         String arr[] = new String[3];
         arr[0] = "가";
         arr[1] = "나";
         arr[2] = "다";
 
-        JFrame frame = new JFrame();
-        frame.setLocation(300, 300);
-        frame.setPreferredSize(new Dimension(300, 300));
-        Container contentPane = frame.getContentPane();
-
-        JPanel panel = new JPanel();
-        JLabel label0 = new JLabel(arr[0], SwingConstants.CENTER);
-        JLabel label1 = new JLabel(arr[1], SwingConstants.CENTER);
-        JLabel label2 = new JLabel(arr[2], SwingConstants.CENTER);
-        Font font = new Font("궁서체", Font.BOLD, 40);
-
-        label1.setFont(font);
-
-        panel.add(label0);
-        panel.add(label1);
-        panel.add(label2);
-        contentPane.add(panel);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("나")) {
+                System.out.print(WHITE_BOLD_BRIGHT + arr[i] + ANSI_RESET);
+            } else {
+                System.out.print(ANSI_WHITE + arr[i] + ANSI_RESET);
+            }
+        }
 
     }
 }
