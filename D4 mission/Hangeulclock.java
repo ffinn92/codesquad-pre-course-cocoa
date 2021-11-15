3d9497888f55f2bc95c899fa3ee6550d7bc4d1f4
ffinn92 <92678171+ffinn92@users.noreply.github.com>
2021-11-15 21:26:47 +0900

    한글 출력보드 배열 표기 수정 및 시간별 출력 함수 구성시작

--- D4 mission/Hangeulclock.java
+++ D4 mission/Hangeulclock.java
@@ -13,58 +13,58 @@ public class Hangeulclock {
         //3.각 시간(숫자)과 한글표기가 일치할때 볼드처리를 해준다.
         //3-1.일치하는지 찾는 방법은 스위치 케이스 문으로 하나씩 확인해본다.
         //2~3번을 반복한다.
-        String[][] boardArr = new String[6][6];
-
-        boardArr[0][0] = "한";
-        boardArr[0][1] = "두";
-        boardArr[0][2] = "세";
-        boardArr[0][3] = "네";
-        boardArr[0][4] = "다";
-        boardArr[0][5] = "섯";
-
-        boardArr[1][0] = "여";
-        boardArr[1][1] = "섯";
-        boardArr[1][2] = "일";
-        boardArr[1][3] = "곱";
-        boardArr[1][4] = "여";
-        boardArr[1][5] = "덟";
-
-        boardArr[2][0] = "아";
-        boardArr[2][1] = "홉";
-        boardArr[2][2] = "열";
-        boardArr[2][3] = "한";
-        boardArr[2][4] = "두";
-        boardArr[2][5] = "시";
-
-        boardArr[3][0] = "자";
-        boardArr[3][1] = "이";
-        boardArr[3][2] = "삼";
-        boardArr[3][3] = "사";
-        boardArr[3][4] = "오";
-        boardArr[3][5] = "십";
-
-        boardArr[4][0] = "정";
-        boardArr[4][1] = "일";
-        boardArr[4][2] = "이";
-        boardArr[4][3] = "삼";
-        boardArr[4][4] = "사";
-        boardArr[4][5] = "육";
-
-        boardArr[5][0] = "오";
-        boardArr[5][1] = "오";
-        boardArr[5][2] = "칠";
-        boardArr[5][3] = "팔";
-        boardArr[5][4] = "구";
-        boardArr[5][5] = "분";
+        String[][] boardArr = {
+                {"한","두","세","네","다","섯"},
+                {"여","섯","일","곱","여","덟"},
+                {"아","홉","열","한","두","시"},
+                {"자","이","삼","사","오","십"},
+                {"정","일","이","삼","사","육"},
+                {"오","오","칠","팔","구","분"}
+        };
 
         LocalDateTime current = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:mm");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:mm a");
         String formatted = current.format(formatter);
 
         System.out.println(formatted.charAt(0));
         System.out.println(formatted.charAt(1));
         System.out.println(formatted.charAt(2));
         System.out.println(formatted.charAt(3));
+
+        System.out.println(formatted.substring(0, 7));
+
+        System.out.println(formatted.substring(0, 1));
+        System.out.println(formatted.substring(2, 4));
+        System.out.println(formatted.substring(5, 7));
+
+        int nowHour = Integer.parseInt(formatted.substring(0,1));
+        String hour = "";
+        switch (nowHour) {
+            case 1: hour = "한시";
+                    break;
+            case 2: hour = "한시";
+                break;
+            case 3: hour = "두시";
+                break;
+            case 4: hour = "세시";
+                break;
+            case 5: hour = "네시";
+                break;
+            case 6: hour = "다섯시";
+                break;
+            case 7: hour = "여섯시";
+                break;
+            case 8: hour = "일곱시";
+                break;
+            case 9: hour = "아홉시";
+                break;
+            case 10: hour = "열시";
+                break;
+            case 11: hour = "열한시";
+                break;
+        }
+
+
 
 
     }
