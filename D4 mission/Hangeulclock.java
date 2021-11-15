9db6a19a483fbcf611d4b71ecda9458d1253135d
ffinn92 <92678171+ffinn92@users.noreply.github.com>
2021-11-15 16:21:57 +0900

    한글 시간출력 보드 우선 구현

--- D4 mission/Hangeulclock.java
+++ D4 mission/Hangeulclock.java
@@ -1,5 +1,4 @@
-import javax.swing.*;
-import java.awt.*;
+
 
 public class Hangeulclock {
     public static final String ANSI_RESET = "\u001B[0m";
@@ -29,17 +28,61 @@ public class Hangeulclock {
         //3.각 시간(숫자)과 한글표기가 일치할때 볼드처리를 해준다.
         //3-1.일치하는지 찾는 방법은 스위치 케이스 문으로 하나씩 확인해본다.
         //2~3번을 반복한다.
+        String[][] boardArr = new String[6][6];
 
-        String arr[] = new String[3];
-        arr[0] = "가";
-        arr[1] = "나";
-        arr[2] = "다";
+        boardArr[0][0] = "한";
+        boardArr[0][1] = "두";
+        boardArr[0][2] = "세";
+        boardArr[0][3] = "네";
+        boardArr[0][4] = "다";
+        boardArr[0][5] = "섯";
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals("나")) {
-                System.out.print(WHITE_BOLD_BRIGHT + arr[i] + ANSI_RESET);
-            } else {
-                System.out.print(ANSI_WHITE + arr[i] + ANSI_RESET);
+        boardArr[1][0] = "여";
+        boardArr[1][1] = "섯";
+        boardArr[1][2] = "일";
+        boardArr[1][3] = "곱";
+        boardArr[1][4] = "여";
+        boardArr[1][5] = "덟";
+
+        boardArr[2][0] = "아";
+        boardArr[2][1] = "홉";
+        boardArr[2][2] = "열";
+        boardArr[2][3] = "한";
+        boardArr[2][4] = "두";
+        boardArr[2][5] = "시";
+
+        boardArr[3][0] = "자";
+        boardArr[3][1] = "이";
+        boardArr[3][2] = "삼";
+        boardArr[3][3] = "사";
+        boardArr[3][4] = "오";
+        boardArr[3][5] = "십";
+
+        boardArr[4][0] = "정";
+        boardArr[4][1] = "일";
+        boardArr[4][2] = "이";
+        boardArr[4][3] = "삼";
+        boardArr[4][4] = "사";
+        boardArr[4][5] = "육";
+
+        boardArr[5][0] = "오";
+        boardArr[5][1] = "오";
+        boardArr[5][2] = "칠";
+        boardArr[5][3] = "팔";
+        boardArr[5][4] = "구";
+        boardArr[5][5] = "분";
+
+
+        for (int i = 0; i < boardArr.length; i++) {
+            for (int j = 0; j < boardArr.length; j++) {
+                if (boardArr[i][j].equals("삼")) {
+                    System.out.print(WHITE_BOLD_BRIGHT + boardArr[i][j] + ANSI_RESET);
+                } else if (j == 5) {
+                    System.out.println(ANSI_WHITE + boardArr[i][j] + ANSI_RESET);
+                } else {
+                    System.out.print(ANSI_WHITE + boardArr[i][j] + ANSI_RESET);
+                }
+
             }
         }
 
