31f3c6be6d1c92bd2ab8c5e6dc35fbda06275240
ffinn92 <92678171+ffinn92@users.noreply.github.com>
2021-11-24 16:25:17 +0900

    메뉴바 상태바 업데이트

--- D18 Mission/GrimPan.java
+++ D18 Mission/GrimPan.java
@@ -4,6 +4,40 @@ public class GrimPan {
     public static void main(String[] args) {
         Frame f = new Frame("그림판");
         f.setSize(1000, 700);
+
+        MenuBar mb = new MenuBar();
+        Menu mFile = new Menu("파일");
+
+        MenuItem miNew = new MenuItem("새로 만들기");
+        MenuItem miOpen = new MenuItem("열기");
+        Menu mOthers = new Menu("그외");
+        MenuItem miExit = new MenuItem("나가기");
+
+        mFile.add(miNew);
+        mFile.add(miOpen);
+        mFile.add(mOthers);
+        mFile.addSeparator();
+        mFile.add(miExit);
+
+        MenuItem miPrint = new MenuItem("인쇄");
+        MenuItem miPreview = new MenuItem("인쇄 미리보기");
+        MenuItem miSetup = new MenuItem("인쇄 설정");
+        mOthers.add(miPrint);
+        mOthers.add(miPreview);
+        mOthers.add(miSetup);
+
+        Menu mEdit = new Menu("설정");
+        Menu mView = new Menu("보기");
+        Menu mHelp = new Menu("도움말");
+        CheckboxMenuItem miStatusbar = new CheckboxMenuItem("상태바");
+        mView.add(miStatusbar);
+
+        mb.add(mFile);
+        mb.add(mEdit);
+        mb.add(mView);
+        mb.setHelpMenu(mHelp);
+
+        f.setMenuBar(mb);
         f.setVisible(true);
     }
 }
