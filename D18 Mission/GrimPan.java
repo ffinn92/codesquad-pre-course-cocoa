00b3cb85d73b15fff9687b1468f2dcc833a2ba1a
ffinn92 <92678171+ffinn92@users.noreply.github.com>
2021-11-24 15:58:18 +0900

    프레임창 구현

---
+++ D18 Mission/GrimPan.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class GrimPan {
+    public static void main(String[] args) {
+        Frame f = new Frame("그림판");
+        f.setSize(1000, 700);
+        f.setVisible(true);
+    }
+}
